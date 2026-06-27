72b6ef768f5ef7c8df1ea30dab2f45c43c2bc14e
Michał Błotniak <michalblotniak@gmail.com>
2026-06-27 17:54:15 +0200

    work in progress

--- firmware/Tasks/Src/communication_task.cpp
+++ firmware/Tasks/Src/communication_task.cpp
@@ -8,10 +8,11 @@
 
 /* Includes ------------------------------------------------------------------*/
 #include "communication_task.h"
-#include "can_interface.hpp"
 #include "cmsis_os2.h"
 #include "data.h"
 #include "fdcan.h"
+#include "PUTM_CAN_M.h"
+#include "can_driver.hpp"
 //#include "iwdg.h"
 
 /* Typedefs ------------------------------------------------------------------*/
@@ -27,98 +28,87 @@ extern osMutexId_t dataMutexHandle;
 
 /* Private function prototypes -----------------------------------------------*/
 
-/* Public function prototypes ------------------------------------------------*/
-
-/* Private functions ---------------------------------------------------------*/
-
 /* Public functions ----------------------------------------------------------*/
-void Communication_Task(void* argument) {
-	//Log_Send(LOG_INFO, "Communication task started");
-	data.rtd_2=-1;
-	data.rtd_2_prev=-1;
 
-	    data.rtd_edge=0;
+void Communication_Task(void* argument)
+{
+    data.rtd_2      = -1;
+    data.rtd_2_prev = -1;
+    data.rtd_edge   =  0;
 
-    for(;;) {
-        // Send
-        PUTM_CAN::RearboxSafety rearbox_safety = {
-            .safety_tsmp = safety.TSMP,
-            .safety_rfu = 0,
-            .safety_hv_battery = safety.hv,
-            .safety_inv_hv = safety.inv_hv,
-            .safety_hvd = safety.hvd,
-            .safety_inv = safety.inv,
-            .safety_wheel_fl = safety.wheel_fl,
-            .safety_wheel_fr = safety.wheel_fr,
-            .safety_wheel_rl = safety.wheel_rl,
-            .safety_wheel_rr = safety.wheel_rr,
-		    .safety_suspension_rl = safety.sus_rl,
-		    .safety_suspension_rr = safety.sus_rr,
-		    .safety_motor_front = safety.motor_front,
-        };
-        auto safety_message = PUTM_CAN::Can_tx_message<PUTM_CAN::RearboxSafety>(rearbox_safety, PUTM_CAN::can_tx_header_REARBOX_SAFETY);
-        safety_message.send(hfdcan1);
+    putm_ev_can::CanDriver can_m;
 
-        PUTM_CAN::RearboxTemperature rearbox_temperature = {
-            .mono_temperature = temperature.mono,
-            .coolant_temperature_in = temperature.coolant_in,
-            .coolant_temperature_out = temperature.coolant_out,
-            .oil_temperature_l = temperature.oil_l,
-            .oil_temperature_r = temperature.oil_r,
-        };
-        auto temperature_message = PUTM_CAN::Can_tx_message<PUTM_CAN::RearboxTemperature>(rearbox_temperature, PUTM_CAN::can_tx_header_REARBOX_TEMPERATURE);
-        temperature_message.send(hfdcan1);
+    if (!can_m.Init(&hfdcan1))
+    {
+        Error_Handler();
+    }
 
-        PUTM_CAN::RearboxMiscellaneous rearbox_miscellaneous = {
-            .coolant_pressure_in = data.coolant_pressure_in,
-            .coolant_pressure_out = data.coolant_pressure_out,
-            .suspension_l = data.suspension_l,
-            .suspension_r = data.suspension_r,
-        };
-        auto miscellaneous_message =
-            PUTM_CAN::Can_tx_message<PUTM_CAN::RearboxMiscellaneous>(rearbox_miscellaneous, PUTM_CAN::can_tx_header_REARBOX_MISCELLANEOUS);
-        miscellaneous_message.send(hfdcan1);
-
-        // Receive
-        if(PUTM_CAN::can.get_pc_new_data()) {
-            auto pc_data = PUTM_CAN::can.get_pc_main_data();
-            if(osMutexAcquire(dataMutexHandle, osWaitForever) == osOK) {
-                data.rtd = pc_data.rtd;
-                data.rtd_2_prev=data.rtd_2;
-                data.rtd_2=data.rtd;
-                if(data.rtd_2_prev==0&& data.rtd_2==1)
-                {
-                	data.rtd_edge =1;
-                }
-                if(data.rtd_2_prev ==0 && data.rtd_2==0 )
-                {
-                	data.rtd_edge=0;
-                }
-                osMutexRelease(dataMutexHandle);
-            }
-        }
-
-        if(PUTM_CAN::can.get_front_data_main_new_data())
-        {
-            auto front_data = PUTM_CAN::can.get_front_data_main_data();
-            //HAL_IWDG_Refresh(&hiwdg);
-            if(osMutexAcquire(dataMutexHandle, osWaitForever) == osOK)
+    can_m.RegisterCallback<PUTM_CAN_M_pc_main_data_t>(
+        PUTM_CAN_M_PC_MAIN_DATA_FRAME_ID,
+        [](const PUTM_CAN_M_pc_main_data_t& pc_main_data) {
+            if (osMutexAcquire(dataMutexHandle, osWaitForever) == osOK)
             {
-                if(front_data.is_braking)
-                {
-                    data.brake_light = true;
-                } else
-                {
-                    data.brake_light = false;
-                }
+                data.rtd        = pc_main_data.rtd;
+                data.rtd_2_prev = data.rtd_2;
+                data.rtd_2      = data.rtd;
+
+                if (data.rtd_2_prev == 0 && data.rtd_2 == 1)
+                    data.rtd_edge = 1;
+
+                if (data.rtd_2_prev == 0 && data.rtd_2 == 0)
+                    data.rtd_edge = 0;
 
                 osMutexRelease(dataMutexHandle);
             }
-        }
+        });
 
-        // Reset watchdog
-       // HAL_IWDG_Refresh(&hiwdg);
+    can_m.RegisterCallback<PUTM_CAN_M_front_data_main_t>(
+        PUTM_CAN_M_FRONT_DATA_MAIN_FRAME_ID,
+        [](const PUTM_CAN_M_front_data_main_t& front_data) {
+            if (osMutexAcquire(dataMutexHandle, osWaitForever) == osOK)
+            {
+                data.brake_light = front_data.is_braking;
+                osMutexRelease(dataMutexHandle);
+            }
+        });
 
+    for (;;)
+    {
+        PUTM_CAN_M_rearbox_safety_t rearbox_safety = {
+            .safety_tsmp          = safety.TSMP,
+            .safety_rfu           = 0,
+            .safety_hv_battery    = safety.hv,
+            .safety_inv_hv        = safety.inv_hv,
+            .safety_hvd           = safety.hvd,
+            .safety_inv           = safety.inv,
+            .safety_wheel_fl      = safety.wheel_fl,
+            .safety_wheel_fr      = safety.wheel_fr,
+            .safety_wheel_rl      = safety.wheel_rl,
+            .safety_wheel_rr      = safety.wheel_rr,
+            .safety_suspension_rl = safety.sus_rl,
+            .safety_suspension_rr = safety.sus_rr,
+            .safety_motor_front   = safety.motor_front,
+        };
+        can_m.Send(PUTM_CAN_M_REARBOX_SAFETY_FRAME_ID, rearbox_safety);
+
+        PUTM_CAN_M_rearbox_temperature_t rearbox_temperature = {
+            .mono_temperature        = temperature.mono,
+            .coolant_temperature_in  = temperature.coolant_in,
+            .coolant_temperature_out = temperature.coolant_out,
+            .oil_temperature_l       = temperature.oil_l,
+            .oil_temperature_r       = temperature.oil_r,
+        };
+        can_m.Send(PUTM_CAN_M_REARBOX_TEMPERATURE_FRAME_ID, rearbox_temperature);
+
+        PUTM_CAN_M_rearbox_miscellaneous_t rearbox_miscellaneous = {
+            .coolant_pressure_in  = data.coolant_pressure_in,
+            .coolant_pressure_out = data.coolant_pressure_out,
+            .suspension_l         = data.suspension_l,
+            .suspension_r         = data.suspension_r,
+        };
+        can_m.Send(PUTM_CAN_M_REARBOX_MISCELLANEOUS_FRAME_ID, rearbox_miscellaneous);
+
+        HAL_IWDG_Refresh(&hiwdg);
         osDelay(100);
     }
 }
